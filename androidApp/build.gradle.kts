89dab87396ceee0dfd4d9a816a86cec4a74beaee
Jatin Vakharia <jatin.vakharia@vuclip.com>
2023-06-28 19:23:48 +0530

    Incremented app version to 1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.hit.chakra"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
