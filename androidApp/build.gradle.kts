d41da7f2a5fb52972224fb4cf964cbf147d17c7d
Jatin Vakharia <jatin.vakharia@vuclip.com>
2023-07-18 23:10:22 +0530

    Modified app version for new release and modified gitIgnore to ignore .aab file

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.hit.chakra"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
