181242c8c2953b55b17039263f8d1f7867ca615b
Jatin Vakharia <jatin.vakharia@vuclip.com>
2023-10-25 19:49:05 +0530

    Modified missed animation, made it more simple

--- shared/src/commonMain/kotlin/ui/MissedAnimation.kt
+++ shared/src/commonMain/kotlin/ui/MissedAnimation.kt
@@ -31,30 +31,8 @@ import utils.playMissedSound
 @Composable
 fun MissedAnimation() {
 
-    val ringScale = remember {
-        Animatable(0f)
-    }
-    val ringOpacity = remember {
-        Animatable(1f)
-    }
     val textScale = remember {
         Animatable(1f)
-    }
-
-    val textRotation = remember {
-        Animatable(0f)
-    }
-
-    val orbOffset = remember {
-        Animatable(0f)
-    }
-
-    val orbScale = remember {
-        Animatable(0f)
-    }
-
-    val color = remember {
-        mutableStateOf(Color.White)
     }
 
     val coroutineScope = rememberCoroutineScope()
@@ -69,7 +47,7 @@ fun MissedAnimation() {
         exit = fadeOut(),
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = SCREEN_HEIGHT_DP.dp / 3 + fixedExtraHeightDP)
+            .padding(top = SCREEN_HEIGHT_DP.dp / 2 + fixedExtraHeightDP)
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -77,101 +55,33 @@ fun MissedAnimation() {
                 .fillMaxWidth()
         ) {
 
-            val offsetEven = with(LocalDensity.current) {
-                orbOffset.value.toDp()
-            }
-            val offsetOdd = with(LocalDensity.current) {
-                orbOffset.value.toDp()
-                    .div(1.2.dp)
-            }
-
-            Box {
-                for (item in 0 until 8) {
-                    Canvas(
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .rotate(item.times(45f))
-                            .offset(
-                                x = 0.dp,
-                                y = (if (item % 2 == 0) offsetEven else offsetOdd.dp)
-                            )
-                            .scale(orbScale.value),
-                        onDraw = {
-                            drawCircle(
-                                color = darkYellow,
-//                                    color = if (item % 2 == 0) Color.Blue else Color.Red,
-                                alpha = if (item % 2 == 0) 1f else 0.5f,
-                                radius = if (item % 2 == 0) 150f else 50f
-                            )
-                        })
-                }
-
-                Canvas(modifier = Modifier
-                    .size(250.dp)
-                    .align(Alignment.Center)
-                    .scale(ringScale.value), onDraw = {
-                    drawCircle(
-                        color = darkYellow.copy(alpha = ringOpacity.value),
-                        style = Stroke(width = 8f)
-                    )
-                })
-
-                Box(contentAlignment = Alignment.Center,
+            Box(modifier = Modifier
+                .padding(top = 150.dp)) {
+                Text(
+                    text = "You Missed",
+                    textAlign = TextAlign.Center,
+                    color = darkYellow,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 30.sp,
                     modifier = Modifier
-                        .wrapContentSize()
+                        .align(Alignment.Center)
                         .size(150.dp)
-                        .background(
-                            color = darkYellow,
-                            shape = CircleShape
-                        )
-                        .align(Alignment.Center),
-                ) {
-                    Text(
-                        text = "You Missed",
-                        textAlign = TextAlign.Center,
-                        color = color.value,
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = 24.sp,
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .size(100.dp)
-                            .scale(textScale.value)
-                            .rotate(textRotation.value)
-                    )
-                }
-
+                        .scale(textScale.value)
+                )
             }
         }
     }
 
     coroutineScope.launch {
-//        delay(500)
         coroutineScope.launch {
-            orbOffset.animateTo(0f)
-            ringOpacity.animateTo(1f)
-            ringScale.animateTo(0f)
             textScale.animateTo(0.6f, tween(500, easing = LinearEasing))
-            textRotation.animateTo(-30f, tween(500, easing = LinearEasing))
         }
 
         coroutineScope.launch {
             delay(300)
             coroutineScope.launch {
-                orbOffset.animateTo(orbOffset.value.minus(250f), tween(1000))
-            }
-            color.value = Color.Black
-            coroutineScope.launch {
-                orbScale.animateTo(1f)
-                ringScale.animateTo(1f)
                 textScale.animateTo(1f)
-                ringOpacity.animateTo(0f)
-                textRotation.animateTo(0f)
             }
-        }
-
-        coroutineScope.launch {
-            delay(300)
-            orbScale.animateTo(0f, tween(1200))
         }
 
         coroutineScope.launch {
